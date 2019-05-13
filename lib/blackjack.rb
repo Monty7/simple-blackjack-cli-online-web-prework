b67f4eaf581e164180cf48921607ce91dacc0cc8
voluble-bug-0219 <montensia1@outlook.com>
2019-05-13 23:25:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,8 +66,8 @@ def runner
   welcome 
   card_total = initial_round
   until card_total > 21
-    hit? card_total
-    card_total += initial_round
+    card_total = hit? card_total
+    
   end
   
 end
